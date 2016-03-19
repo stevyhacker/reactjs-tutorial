3acc700cbaca9882a3c7fc46ea6d8f72e669a4ed
Stevan Bogosavljević <stevan_nk@ymail.com>
2016-03-19 20:51:58 +0100

    React setup

---
+++ app/components/Main.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Main = React.createClass({
+
+    render: function () {
+        return (
+            <div>
+                Hello react!!!
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Main/>,document.getElementById("app"));
+
+
